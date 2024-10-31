76d82d813b46d158c3727182d4a5b0fcc8da9dcb
pedrocruz <JayJMarquez05@gmail.com>
2024-10-31 13:01:19 -0500

    My todo is now linked onto the button! :D

--- src/components/NewTodo.jsx
+++ src/components/NewTodo.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 const NewTodo = (props) => {
+  const [newTodo, setNewTodo] = useState("");
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    props.addTodo("Hey, we're adding a new todo mate!");
     console.log("Creating a new to-do...");
+  };
+
+  const handleChangeTodo = (event) => {
+    setNewTodo(event.target.value);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="todoInput">Todo: </label>
-      <input id="todoInput" type="text" name="todo" />
+      <input
+        id="todoInput"
+        type="text"
+        name="todo"
+        value={newTodo}
+        onChange={handleChangeTodo}
+      />
       <button type="submit">Create to-do</button>
     </form>
   );
